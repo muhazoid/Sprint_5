11c58955ca5b282a06114e03015eb632347d3cb5
Alexey Chikichev <agentlich2012@gmail.com>
2026-03-18 19:05:12 +0700

    add registr_test

---
+++ tests/locators.py
@@ -0,0 +1,18 @@
+from selenium.webdriver.common.by import By
+
+
+class RegisterPageLocators:
+    FIELD_NAME = (By.XPATH, './/label[text()="Имя"]//parent::*/input') # Поле ввода Имени
+    FIELD_EMAIL = (By.XPATH, './/label[text()="Email"]//parent::*/input') # Поле ввода Email
+    FIELD_PASSWORD = (By.XPATH, './/label[text()="Пароль"]//parent::*/input') # Поле ввода Пароля
+    BUTTON_REGISTER = (By.XPATH, ".//button[text()='Зарегистрироваться']") # Кнопка "Зарегистрироваться"
+    ERROR_INCORRECT_PASSWORD = (By.XPATH, ".//p[text()='Некорректный пароль']") # Ошибка "Некорректный пароль"
+
+class Urls:
+    SERVICE_BASE_URL = 'https://stellarburgers.education-services.ru'
+    main_page = f'{SERVICE_BASE_URL}/'
+    registration_page = f'{SERVICE_BASE_URL}/register'
+    login_page = f'{SERVICE_BASE_URL}/login'
+    profile_page = f'{SERVICE_BASE_URL}/account/profile'
+    order_history_page = f'{SERVICE_BASE_URL}/account/order-history'
+    forgot_password_page = f'{SERVICE_BASE_URL}/forgot-password'
